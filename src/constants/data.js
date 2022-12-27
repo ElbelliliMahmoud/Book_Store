f898a03dcc941446b498f93098e9f17a46012a10
elbellili <mahmoud.elbellili@3dfordeco.com>
2022-12-27 16:09:50 +0100

    update data file

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,47 +1,6 @@
 import images from './images'
 
-const books = [
-  {
-    index: 0,
-    imgUrl: images.book1,
-  },
-  {
-    index: 1,
-    imgUrl: images.book2,
-  },
-  {
-    index: 2,
-    imgUrl: images.book3,
-  },
-  {
-    index: 3,
-    imgUrl: images.book4,
-  },
-  {
-    index: 4,
-    imgUrl: images.book5,
-  },
-  {
-    index: 5,
-    imgUrl: images.book6,
-  },
-  {
-    index: 6,
-    imgUrl: images.book7,
-  },
-  {
-    index: 7,
-    imgUrl: images.book8,
-  },
-  {
-    index: 8,
-    imgUrl: images.book9,
-  },
-  {
-    index: 9,
-    imgUrl: images.book10,
-  },
-];
+
 
 const containers = [
   {
@@ -70,72 +29,7 @@ const containers = [
   },
 ];
 
-const cards = [
-  {
-    index: 0,
-    imgUrl: images.book1,
-    title: "featured books",
-    new_price: "$15.99",
-    old_price: "$20.99",
-    rating: 3.5,
-  },
-  {
-    index: 1,
-    imgUrl: images.book2,
-    title: "featured books",
-    new_price: "$15.99",
-    old_price: "$20.99",
-    rating: 4.5,
-  },
-  {
-    index: 2,
-    imgUrl: images.book3,
-    title: "featured books",
-    new_price: "$15.99",
-    old_price: "$20.99",
-    rating: 4,
-  },
-  {
-    index: 3,
-    imgUrl: images.book4,
-    title: "featured books",
-    new_price: "$15.99",
-    old_price: "$20.99",
-    rating: 5,
-  },
-  {
-    index: 4,
-    imgUrl: images.book5,
-    title: "featured books",
-    new_price: "$15.99",
-    old_price: "$20.99",
-    rating: 3.5,
-  },
-  {
-    index: 5,
-    imgUrl: images.book6,
-    title: "featured books",
-    new_price: "$15.99",
-    old_price: "$20.99",
-    rating: 3.5,
-  },
-  {
-    index: 6,
-    imgUrl: images.book7,
-    title: "featured books",
-    new_price: "$15.99",
-    old_price: "$20.99",
-    rating: 3.5,
-  },
-  {
-    index: 7,
-    imgUrl: images.book8,
-    title: "featured books",
-    new_price: "$15.99",
-    old_price: "$20.99",
-    rating: 3.5,
-  },
-];
+
 
 const reviews = [
   {
